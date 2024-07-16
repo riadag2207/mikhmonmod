17182240665017ee4a3e06c296fde349dfbc8e98
Naufal Azkia <ztgsk8@gmail.com>
2024-07-16 12:17:40 +0700

    Initial commit

---
+++ src/webhook/dvoucher.php
@@ -0,0 +1,8 @@
+interhome*
+1-Hari|1 Hari|4000|4000|1d|interhome|1M/2M*
+2-Hari|2 Hari|7000|7000|2d|interhome|1M/2M*
+8Jam|8 Jam|2000|2000|8h|interhome|1M/2M*
+12jam|12 Jam|3000|3000|12h|interhome|1M/2M*
+1-Hari-Paade|1 Hari|3000|3000|1d|interhome|850K/1500K*
+2-Hari-Paade|2 Hari|5000|5000|2d|interhome|850K/1500K#
+
---
+++ src/webhook/webhookk.php
@@ -0,0 +1,1 @@
+5337580581|titawatiya|skynetplays_bot|5982153422:AAEmm0WJ_XvSHq-tJaCPqxyPBYU1KpPlmRs|https://interhome.wifikonekter.us/webhook/Core.php|
